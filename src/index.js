b6410fe217e8ee7c8e7651981cd9672c821773c5
krish7kumar <krish98897@gmail.com>
2020-07-09 17:49:27 +0530

    Relaxeye-reactApp

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/relaxeye/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
