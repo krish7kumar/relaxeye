4d696c603e2c1cd38bb77f843419556d1a437f0f
krish7kumar <krish98897@gmail.com>
2020-07-09 18:17:28 +0530

    Relaxeye-reactApp

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-    <HashRouter>
+    <HashRouter basename='/relaxeye'>
       <App />
     </HashRouter>,
   document.getElementById('root')
