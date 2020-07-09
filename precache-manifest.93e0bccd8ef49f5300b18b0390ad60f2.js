0e30fd00b53dd59802ac7e00b68d376cb2636539
krish7kumar <krish98897@gmail.com>
2020-07-09 17:42:45 +0530

    Updates

---
+++ precache-manifest.93e0bccd8ef49f5300b18b0390ad60f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9795f3a6226c8a6bbd16a12474a035d0",
+    "url": "/krish7kumar/relaxeye/index.html"
+  },
+  {
+    "revision": "23ca4a81b159dbb4b6a0",
+    "url": "/krish7kumar/relaxeye/static/css/main.568b99e2.chunk.css"
+  },
+  {
+    "revision": "bf1153b34f2bf218f2b3",
+    "url": "/krish7kumar/relaxeye/static/js/2.5ad3ba00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/krish7kumar/relaxeye/static/js/2.5ad3ba00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ca4a81b159dbb4b6a0",
+    "url": "/krish7kumar/relaxeye/static/js/main.517778a3.chunk.js"
+  },
+  {
+    "revision": "c01f0bd7fd708aab7279",
+    "url": "/krish7kumar/relaxeye/static/js/runtime-main.275adef1.js"
+  }
+]);
