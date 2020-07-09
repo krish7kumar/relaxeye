cabf1f843e24c3dea15047268eb9102fcf16b9e0
krish7kumar <krish98897@gmail.com>
2020-07-09 20:04:15 +0530

    React app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Landing from './Components/Landing/Landing';
 import Exercises from './Containers/Exercises/Exercises';
-import {Route, Switch} from 'react-router-dom';
+import {HashRouter,Route, Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <HashRouter basename='/relaxeye'>
       <Switch>
         <Route path='/exercises' component={Exercises} />
         <Route path='/' exact component={Landing} />
       </Switch>
+      </HashRouter>
     </div>
   );
 }
