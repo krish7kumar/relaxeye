ac2aa91c18349bef231a363fbcd9f0a907440a85
krish7kumar <krish98897@gmail.com>
2020-07-09 18:06:45 +0530

    Relaxeye-reactApp

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter basename='/relaxeye/'>
+    <BrowserRouter basename='/krish7kumar/relaxeye/'>
       <App />
     </BrowserRouter>,
   document.getElementById('root')
